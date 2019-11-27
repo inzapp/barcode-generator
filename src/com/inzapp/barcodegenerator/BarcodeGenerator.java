bc0eed51e84b5aec0a6bb0e46875223e89af1379
inzapp <keg9903@naver.com>
2019-11-27 11:43:22 +0900

    add package, format jpg to png

--- src/com/inzapp/barcodegenerator/BarcodeGenerator.java
+++ src/com/inzapp/barcodegenerator/BarcodeGenerator.java
@@ -1,3 +1,5 @@
+package com.inzapp.barcodegenerator;
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
@@ -17,12 +19,12 @@ public class BarcodeGenerator {
     public BufferedImage generate(String content) {
         try {
             BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.CODE_128, this.barcodeWidth, 1);
-            BufferedImage bufferedImage = new BufferedImage(this.barcodeWidth, this.barcodeHeight, BufferedImage.TYPE_BYTE_GRAY);
+            BufferedImage bufferedImage = new BufferedImage(this.barcodeWidth, this.barcodeHeight, BufferedImage.TYPE_4BYTE_ABGR);
             for (int x = 0; x < 1080; x++) {
                 for (int y = 0; y < this.barcodeHeight; ++y)
                     bufferedImage.setRGB(x, y, bitMatrix.get(x, 0) ? Byte.MIN_VALUE : Byte.MAX_VALUE);
             }
-            ImageIO.write(bufferedImage, "jpg", new File("barcode.jpg"));
+            ImageIO.write(bufferedImage, "png", new File("barcode.png"));
             return bufferedImage;
         } catch (Exception e) {
             e.printStackTrace();
